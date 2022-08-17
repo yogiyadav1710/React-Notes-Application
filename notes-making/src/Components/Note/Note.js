67c0c155f766178d5a7a430f0f9c885a6f1e90e5
yogeshya <yogeshya@cybage.com>
2022-08-17 15:39:39 +0530

    note component is created

---
+++ notes-making/src/Components/Note/Note.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Note() {
+  return (
+    <div className='note'>
+        <textarea className='note_text'></textarea>
+        <p >10:18 PM 22 August</p>
+    </div>
+  )
+}
+
+export default Note
---
+++ notes-making/src/Components/NoteContainer/NoteContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Note from '../Note/Note'
+
+function NoteContainer() {
+  return (
+    <div>
+        <Note/>
+    </div>
+  )
+}
+
+export default NoteContainer
